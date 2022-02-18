dfdfe74825562d08a5c14c303cb335c3f12a9b36
aewhitfield90 <66714173+aewhitfield90@users.noreply.github.com>
2022-02-18 00:00:32 -0800

    Update div.hpp

--- div.hpp
+++ div.hpp
@@ -11,7 +11,7 @@ public:
     Div(Base* op1, Base* op2) : Base() {
         temp1 = op1;
         temp2 = op2;
-        if (op2->evaluate() == 0) { throw std::invalid_argument("divide by zero."); }
+        //if (op2->evaluate() == 0) { throw std::invalid_argument("divide by zero."); }
     }
     virtual double evaluate() { return temp1->evaluate() / temp2->evaluate(); }
     virtual std::string stringify() { return '(' + temp1->stringify() + "/" + temp2->stringify() + ')'; }
