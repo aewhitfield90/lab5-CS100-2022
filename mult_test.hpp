f43668f5f6dd0dd38379b8f14fca17bb1c8f8158
aewhitfield90 <66714173+aewhitfield90@users.noreply.github.com>
2022-02-18 01:12:18 -0800

    Update mult_test.hpp

--- mult_test.hpp
+++ mult_test.hpp
@@ -7,27 +7,27 @@
 #include "add.hpp"
 
 TEST(MultTest, MultiplyTwoOperators) {
-    Base* five = new Op(5);
-    Base* seven = new Op(7);
-    Base* test = new Mult(five, seven);
+    Op* five = new Op(5);
+    Op* seven = new Op(7);
+    Mult* test = new Mult(five, seven);
     EXPECT_EQ(test->evaluate(), 35);
     delete five, seven, test;
 }
 
 TEST(MultTest, MultiplyOperatorZero) {
-    Base* zero = new Op(0);
-    Base* seven = new Op(7);
-    Base* test = new Mult(zero, seven);
+    Op* zero = new Op(0);
+    Op* seven = new Op(7);
+    Mult* test = new Mult(zero, seven);
     EXPECT_EQ(test->evaluate(), 0);
     delete zero, seven, test;
 }
 
 TEST(MultTest, MultiplyOperatorOperand) {
-    Base* five = new Op(5);
-    Base* seven = new Op(7);
-    Base* sum = new Add(five, seven)
+    Op* five = new Op(5);
+    Op* seven = new Op(7);
+    Add* sum = new Add(five, seven)
     EXPECT_EQ(sum->evaluate(), 12);
-    Base* test = new Mult(five, sum);
+    Mult* test = new Mult(five, sum);
     EXPECT_EQ(test->evaluate(), 60);
     delete five, seven, sum, test;
 }
