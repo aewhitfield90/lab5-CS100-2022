6d8ad5b9ff2897b0a864f30165b8077612127b6d
AlexPNguyen414 <nnguy414@ucr.edu>
2022-02-18 02:18:22 -0800

    added add tests

---
+++ add_test.hpp
@@ -0,0 +1,43 @@
+#ifndef __ADD_TEST_HPP__
+#define __ADD_TEST_HPP__
+
+#include "gtest/gtest.h"
+
+#include "add.hpp"
+#include "op.hpp"
+
+TEST(AddTest, AddPosPos) {
+    Op* nine = new Op(9);
+    Op* ten = new Op(10);
+    Add* test = new Add(nine, ten);
+    EXPECT_EQ(test->evaluate(), 18);
+    delete nine, ten, test;
+}
+
+TEST(AddTest, AddPosNeg) {
+    Op* two = new Op(2);
+    Op* neg_seven = new Op(-7);
+    Add* test = new Add(two, neg_seven);
+    EXPECT_EQ(test->evaluate(), 5);
+    delete two, neg_seven, test;
+}
+
+TEST(AddTest, AddPosZero) {
+    Op* four = new Op(4);
+    Op* zero = new Op(0);
+    Add* test = new Add(four, zero);
+    EXPECT_EQ(test->evaluate(), 4);
+    delete four, zero, test;
+}
+
+TEST(AddTest, AddOperatorOperand) {
+    Op* five = new Op(5);
+    Op* seven = new Op(7);
+    Add* sum = new Add(five, seven);
+    EXPECT_EQ(sum->evaluate(), 12);
+    Add* test = new Add(five, sum);
+    EXPECT_EQ(test->evaluate(), 18);
+    delete five, seven, sum, test;
+}
+
+#endif //__ADD_TEST_HPP__
